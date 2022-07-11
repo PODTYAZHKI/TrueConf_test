6246af9d9a6e7fa772389788ba7a62e170e88a68
Sergey Muzychuk <supper.ser2001@gmail.com>
2022-07-11 17:08:31 +0300

    add basic settings

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <LiftShaft />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LiftShaft from './components/LiftShaft.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+    LiftShaft
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  #app {
+    margin: 20px;
+    height: 100vh;
+    //width: 100vw;
+  }
 </style>
+
+
