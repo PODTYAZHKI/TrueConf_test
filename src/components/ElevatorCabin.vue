01274854436d58b26316da5fe98a89fa601c6ac2
Sergey Muzychuk <supper.ser2001@gmail.com>
2022-07-11 23:04:19 +0300

    add elevator cabin

---
+++ src/components/ElevatorCabin.vue
@@ -0,0 +1,28 @@
+<template>
+  <div ref="cabin" id="cabin" class="cabin" :width="props.width" :height="props.height"> 
+    3
+  </div>
+
+</template>
+
+<script setup>
+import { defineProps, onMounted } from 'vue'
+const props = defineProps({
+  width: Number,
+  height: Number,
+})
+// const cabin = ref(null)
+onMounted(()=>{
+  document.getElementById("cabin").style.height = `${props.height}px`
+  document.getElementById("cabin").style.width = `${props.width}px`
+})
+
+</script>
+
+<style scope lang="scss">
+  .cabin {
+    background-color: red;
+    //display: inline-block;
+    border: 1px solid grey;
+  }
+</style>
