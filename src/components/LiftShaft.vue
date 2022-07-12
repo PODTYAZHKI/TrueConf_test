e5316f8979743cd567017fcf867cef72c040c09d
Sergey Muzychuk <supper.ser2001@gmail.com>
2022-07-12 23:28:31 +0300

    fix indicator colors and add saving states

--- src/components/LiftShaft.vue
+++ src/components/LiftShaft.vue
@@ -15,7 +15,7 @@
       <div class="info-wrapper">
         <div class="info-floor" v-for="i in floors" :key="i">
           {{ i }}
-          <div class="button-wrapper">
+          <div class="button-wrapper" ref="radio_wrapper">
             <input ref="radio" type="radio" checked='True' class="radio" @click="check_lift_queue(i)" />
 
           </div>
@@ -24,7 +24,7 @@
     </div>
 
     <Elevator_cabin class="cabin" :width="shaft_width" :height="floor_height" :floor="toFloor"
-      :direction="cabin_direction" :blink="cabin_blink"/>
+      :direction="cabin_direction" :blink="cabin_blink" />
     <!-- <Building_setting class="settings" /> -->
   </div>
 
@@ -43,8 +43,9 @@ const floor_shaft = ref()
 const shaft_width = ref(null)
 const floor_height = ref(null)
 // const cabin_y = ref('0px')
-const cabin_floor = ref(1)
+const cabin_floor = ref()
 const radio = ref()
+const radio_wrapper = ref()
 const cabin_direction = ref('')
 // const lift_cabin = ref()
 
@@ -58,9 +59,16 @@ function getFloorHeight(element) {
 
 const lift_queue = ref([])
 const cabin_status = ref('ready')
+function queueColor(i) {
+  radio_wrapper.value[i - 1].style.border = '1px solid red'
+    radio.value[i - 1].className = 'call'
+}
 function check_lift_queue(i) {
   if ((lift_queue.value.length === 0 || lift_queue.value.at(-1) != i) && (i != cabin_floor.value || movingFloor.value != i)) {
     lift_queue.value.push(i)
+    localStorage.setItem('queue', lift_queue.value)
+    console.log(store.getters.getCabinQueue)
+    queueColor(i)
     console.log('lift_queue.value', lift_queue.value)
     if (cabin_status.value === 'ready') {
       call()
@@ -68,7 +76,7 @@ function check_lift_queue(i) {
   }
 
 }
-const movingFloor = ref()
+const movingFloor = ref(1)
 const cabin_blink = ref()
 async function call() {
   cabin_status.value = 'moving'
@@ -93,9 +101,14 @@ async function call() {
       console.log('status', cabin_status.value)
       toFloor.value = ''
       cabin_direction.value = ''
+      radio_wrapper.value[movingFloor.value - 1].style.border = '1px solid green'
+
+      radio.value[movingFloor.value - 1].classList.remove("call")
       if (lift_queue.value.length != 0) {
         call()
 
+      } else {
+        localStorage.removeItem('queue')
       }
     }, 3000)
   })
@@ -130,6 +143,7 @@ async function elevatorMove(floor, time) {
   await promise.then(() => {
     // console.log(floor)
     cabin_floor.value = floor
+    localStorage.setItem('cabin_floor', floor)
   })
 
 
@@ -145,10 +159,22 @@ onMounted(() => {
   // console.log(element.clientHeight)
   getshaftWidth(element)
   getFloorHeight(element)
-  // store.commit('setCabinWidth', shaft_width.value)
-  // await store.dispatch('changeCabinWidth', shaft_width.value)
-  // store.commit('setCabinHeight', floor_height.value)
+  if (localStorage.getItem('cabin_floor') != null){
+    cabin_floor.value = localStorage.getItem('cabin_floor')
+    var cabin = document.getElementById('cabin');
+    cabin.style.transform = `translateY(-${(cabin_floor.value - 1) * floor_height.value}px)`;
 
+  } else {
+    cabin_floor.value = 1
+  }
+  if (localStorage.getItem('queue') != null){
+    lift_queue.value = localStorage.getItem('queue').split(',')
+    if (lift_queue.value[0] === cabin_floor.value) {
+      lift_queue.value.shift()
+    }
+    console.log(lift_queue.value)
+    setTimeout(call, 1000)
+  }
 })
 
 </script>
@@ -241,9 +267,10 @@ onMounted(() => {
 .button {
   &-wrapper {
     margin-top: 10px;
+    padding-top: 1px;
     width: 20px;
     height: 20px;
-    border: 1px solid #D8DDDD;
+    border: 1px solid green;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -256,20 +283,58 @@ onMounted(() => {
       cursor: pointer;
     }
 
-    .radio:checked {
-      content: "";
-      position: absolute;
-      -webkit-transition: all 0.2s ease;
-      -moz-transition: all 0.2s ease;
-      -o-transition: all 0.2s ease;
-      transition: all 0.2s ease;
-      left: 5px;
-      top: 5px;
-      width: 10px;
-      height: 10px;
-      border-radius: 100%;
-      background-color: #e145a3;
-    }
+
+
   }
+}
+
+input[type="radio"] {
+  /* Add if not using autoprefixer */
+  -webkit-appearance: none;
+  /* Remove most all native input styles */
+  appearance: none;
+  /* For iOS < 15 */
+  background-color: white;
+  /* Not removed via appearance */
+  margin: 0;
+
+  font: inherit;
+  color: currentColor;
+  width: 1.15em;
+  height: 1.15em;
+  border: 0.15em solid currentColor;
+  border-radius: 50%;
+  transform: translateY(-0.075em);
+
+  display: grid;
+  place-content: center;
+}
+
+input[type="radio"]::before {
+  content: "";
+  width: 0.65em;
+  height: 0.65em;
+  border-radius: 50%;
+  transform: scale(0);
+  transition: 120ms transform ease-in-out;
+  box-shadow: inset 1em 1em green;
+  /* Windows High Contrast Mode */
+  background-color: CanvasText;
+}
+
+input[type="radio"].call::before {
+  content: "";
+  width: 0.65em;
+  height: 0.65em;
+  border-radius: 50%;
+  transform: scale(0);
+  transition: 120ms transform ease-in-out;
+  box-shadow: inset 1em 1em red;
+  /* Windows High Contrast Mode */
+  background-color: CanvasText;
+}
+
+input[type="radio"]:checked::before {
+  transform: scale(1);
 }
 </style>
