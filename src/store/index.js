d8b97df6187c2a13c8824a8e8465fb7c66387a38
Sergey Muzychuk <supper.ser2001@gmail.com>
2022-07-12 12:41:19 +0300

    add store

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,38 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    cabin_width: 0,
+    cabin_height: 0,
+    cabin_y: 0,
+    cabin_floor: 1
   },
   getters: {
+    getCabinWidth(state) {
+      return state.cabin_width
+    },
+    getCabinHeight(state) {
+      return state.cabin_height
+    },
+    getCabinY(state) {
+      return state.cabin_y
+    },
+    getCabinFloor(state) {
+      return state.cabin_floor
+    }
   },
   mutations: {
+    setCabinWidth(state, cabin_width) {
+      state.cabin_width = cabin_width
+    },
+    setCabinHeight(state, cabin_height) {
+      state.cabin_height = cabin_height
+    },
+    setCabinY(state, cabin_y) {
+      state.cabin_y = cabin_y
+    },
+    setCabinFloor(state, cabin_floor) {
+      state.cabin_floor = cabin_floor
+    },
   },
   actions: {
   },
