0294bba32e0618907a3118c4f164a8484d2110e6
Sergey Muzychuk <supper.ser2001@gmail.com>
2022-07-12 19:47:04 +0300

    fix cabin size

--- src/components/ElevatorCabin.vue
+++ src/components/ElevatorCabin.vue
@@ -1,19 +1,21 @@
 <template>
-  <div ref="cabin" id="cabin" class="cabin"> 
-    
+  <div ref="cabin" id="cabin" class="cabin">
+
   </div>
 
 </template>
 
 <script setup>
-import {  onMounted } from 'vue'
+import { onMounted } from 'vue'
 import { useStore } from 'vuex'
 const store = useStore()
 
 // const cabin = ref(null)
-onMounted(()=>{
-  document.getElementById("cabin").style.height = `${store.getters.getCabinHeight}px`
-  document.getElementById("cabin").style.width = `${store.getters.getCabinWidth}px`
+onMounted(() => {
+  setTimeout(() => {
+    document.getElementById("cabin").style.height = `${store.getters.getCabinHeight}px`
+    document.getElementById("cabin").style.width = `${store.getters.getCabinWidth}px`
+  }, 1000)
   // console.log(store.getters.getCabinHeight)
   // console.log(store.getters.getCabinWidth)
   // console.log(document.getElementById("cabin").style.width)
@@ -24,9 +26,9 @@ onMounted(()=>{
 </script>
 
 <style scope lang="scss">
-  .cabin {
-    background-color: red;
-    //display: inline-block;
-    //border: 1px solid grey;
-  }
+.cabin {
+  background-color: red;
+  //display: inline-block;
+  //border: 1px solid grey;
+}
 </style>
