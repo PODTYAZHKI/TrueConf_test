2816b8baf93a888eb958d9fe6b8fde79cda18369
Sergey Muzychuk <supper.ser2001@gmail.com>
2022-07-12 21:46:52 +0300

    add indicators for floor and direction

--- src/components/ElevatorCabin.vue
+++ src/components/ElevatorCabin.vue
@@ -1,21 +1,46 @@
 <template>
-  <div ref="cabin" id="cabin" class="cabin">
-
+  <!-- <div id="cabin_wrapper" class="cabin_wrapper"> -->
+  <div id="cabin" class="cabin" :class="[props.direction === '' ? '' : 'blink']">
+    <div class="cabin_indicator_wrapper">
+      <div class="cabin_indicator"  :class="[props.direction === '' ? '' : 'cabin_indicator_move']">
+        <div class="direction">
+          {{ props.direction }}
+        </div>
+        {{ props.floor }}
+      </div>
+    </div>
   </div>
+  <!-- </div> -->
 
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
-import { useStore } from 'vuex'
-const store = useStore()
+import { onMounted, defineProps } from 'vue'
+// import { useStore } from 'vuex'
+// const store = useStore()
+// const floor = ref()
+
+const props = defineProps({
+  width: String,
+  height: String,
+  floor: Number,
+  direction: String
+});
 
 // const cabin = ref(null)
+
+// function setFloor(i) {
+//   floor.value = i
+// }
 onMounted(() => {
   setTimeout(() => {
-    document.getElementById("cabin").style.height = `${store.getters.getCabinHeight}px`
-    document.getElementById("cabin").style.width = `${store.getters.getCabinWidth}px`
-  }, 1000)
+    // document.getElementById("cabin").style.height = `${store.getters.getCabinHeight}px`
+    // document.getElementById("cabin").style.width = `${store.getters.getCabinWidth}px`
+    document.getElementById("cabin").style.height = `${props.height}px`
+    document.getElementById("cabin").style.width = `${props.width}px`
+    // document.getElementById("cabin_wrapper").style.height = `${props.height}px`
+    // document.getElementById("cabin_wrapper").style.width = `${props.width}px`
+  }, 300)
   // console.log(store.getters.getCabinHeight)
   // console.log(store.getters.getCabinWidth)
   // console.log(document.getElementById("cabin").style.width)
@@ -27,8 +52,52 @@ onMounted(() => {
 
 <style scope lang="scss">
 .cabin {
-  background-color: red;
+  background-color: #03FFFF;
+
+
+  &_wrapper {
+    background-color: white;
+    //width: 100%;
+    //height: 100%;
+  }
+
   //display: inline-block;
   //border: 1px solid grey;
+  &_indicator {
+    background-color: #139897;
+    width: 40px;
+    border-radius: 10%;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &_move {
+      padding-top: 5px;
+      padding-bottom: 5px;
+    }
+
+    //padding-top: 5px;
+    //padding-bottom: 5px;
+    &_wrapper {
+      margin-top: 7px;
+      width: 100%;
+      text-align: center;
+      display: flex;
+      justify-content: center;
+
+
+    }
+  }
+}
+
+.blink {
+  animation: blinker 1s linear infinite;
+}
+
+@keyframes blinker {
+  50% {
+    opacity: 0.5;
+  }
 }
 </style>
