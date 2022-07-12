9704243e65f86c8bc2e685522204c5fe98f682f0
Sergey Muzychuk <supper.ser2001@gmail.com>
2022-07-12 23:27:17 +0300

    clear from //

--- src/components/ElevatorCabin.vue
+++ src/components/ElevatorCabin.vue
@@ -1,8 +1,8 @@
 <template>
-  <!-- <div id="cabin_wrapper" class="cabin_wrapper"> -->
+
   <div id="cabin" class="cabin" :class="[props.blink ? 'blink' : '']">
     <div class="cabin_indicator_wrapper">
-      <div class="cabin_indicator"  :class="[props.direction === '' ? '' : 'cabin_indicator_move']">
+      <div class="cabin_indicator" :class="[props.direction === '' ? '' : 'cabin_indicator_move']">
         <div class="direction">
           {{ props.direction }}
         </div>
@@ -10,43 +10,25 @@
       </div>
     </div>
   </div>
-  <!-- </div> -->
 
 </template>
 
 <script setup>
 import { onMounted, defineProps } from 'vue'
-// import { useStore } from 'vuex'
-// const store = useStore()
-// const floor = ref()
 
 const props = defineProps({
-  width: String,
-  height: String,
-  floor: Number,
+  width: Number,
+  height: Number,
+  floor: String,
   direction: String,
   blink: Boolean
 });
 
-// const cabin = ref(null)
-
-// function setFloor(i) {
-//   floor.value = i
-// }
 onMounted(() => {
   setTimeout(() => {
-    // document.getElementById("cabin").style.height = `${store.getters.getCabinHeight}px`
-    // document.getElementById("cabin").style.width = `${store.getters.getCabinWidth}px`
     document.getElementById("cabin").style.height = `${props.height}px`
     document.getElementById("cabin").style.width = `${props.width}px`
-    // document.getElementById("cabin_wrapper").style.height = `${props.height}px`
-    // document.getElementById("cabin_wrapper").style.width = `${props.width}px`
   }, 300)
-  // console.log(store.getters.getCabinHeight)
-  // console.log(store.getters.getCabinWidth)
-  // console.log(document.getElementById("cabin").style.width)
-  // console.log(document.getElementById("cabin").style.height)
-
 })
 
 </script>
@@ -58,12 +40,8 @@ onMounted(() => {
 
   &_wrapper {
     background-color: white;
-    //width: 100%;
-    //height: 100%;
   }
 
-  //display: inline-block;
-  //border: 1px solid grey;
   &_indicator {
     background-color: #139897;
     width: 40px;
@@ -78,16 +56,12 @@ onMounted(() => {
       padding-bottom: 5px;
     }
 
-    //padding-top: 5px;
-    //padding-bottom: 5px;
     &_wrapper {
       margin-top: 7px;
       width: 100%;
       text-align: center;
       display: flex;
       justify-content: center;
-
-
     }
   }
 }
