18910aa2bf5e0386d40002e70e36f05d77328c91
Sergey Muzychuk <supper.ser2001@gmail.com>
2022-07-12 23:25:40 +0300

    rewrite store

--- src/store/index.js
+++ src/store/index.js
@@ -1,56 +1,19 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    cabin_width: 10,
-    cabin_height: 10,
-    cabin_y: 0,
-    cabin_floor: 1,
-
     shafts_count: 1,
     floors_count: 5,
-    
   },
   getters: {
-    getCabinWidth(state) {
-      return state.cabin_width
-    },
-    getCabinHeight(state) {
-      return state.cabin_height
-    },
     getShaftsCount(state) {
-      return state.shafts_count
+      return state.shafts_count;
     },
     getFloorsCount(state) {
-      return state.floors_count
-    }
-    // getCabinY(state) {
-    //   return state.cabin_y
-    // },
-    // getCabinFloor(state) {
-    //   return state.cabin_floor
-    // }
-  },
-  mutations: {
-    setCabinWidth(state, cabin_width) {
-      state.cabin_width = cabin_width
+      return state.floors_count;
     },
-    setCabinHeight(state, cabin_height) {
-      state.cabin_height = cabin_height
-    },
-    // setCabinY(state, cabin_y) {
-    //   state.cabin_y = cabin_y
-    // },
-    // setCabinFloor(state, cabin_floor) {
-    //   state.cabin_floor = cabin_floor
-    // },
   },
-  actions: {
-    // changeCabinWidth({ commit }, payload) {
-    //   commit('setCabinWidth', payload)
-    //   // console.log(123)
-    // },
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {},
+});
