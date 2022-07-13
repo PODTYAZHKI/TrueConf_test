bc9accbc281bda0f60bbd738e60e260095d1602d
Sergey Muzychuk <supper.ser2001@gmail.com>
2022-07-13 13:58:21 +0300

    add multi

--- src/components/LiftShaft.vue
+++ src/components/LiftShaft.vue
@@ -7,7 +7,7 @@
       </div>
     </div>
     <div class="shafts">
-      <div class="shaft" v-for="j in shafts" :key="j">
+      <div ref="shaft" :id='`shaft${j}`' class="shaft" v-for="j in shafts" :key="j">
         <div class="floor_shaft" ref="floor_shaft" v-for="i in floors" :key="i">
 
         </div>
@@ -23,8 +23,9 @@
       </div>
     </div>
 
-    <Elevator_cabin class="cabin" :width="shaft_width" :height="floor_height" :floor="toFloor"
-      :direction="cabin_direction" :blink="cabin_blink" />
+    <Elevator_cabin v-for="j in shafts" :key="j" :id='`cabin${j}`' class="cabin" :width="shaft_width"
+      :height="floor_height" :floor="moving_floor[j - 1]" :direction="cabin_direction[j - 1]"
+      :blink="cabins_blink[j - 1]" />
     <!-- <Building_setting class="settings" /> -->
   </div>
 
@@ -39,17 +40,17 @@ import Elevator_cabin from './ElevatorCabin.vue'
 const store = useStore()
 const floors = ref()
 const shafts = ref()
+const shaft = ref()
 const floor_shaft = ref()
 const shaft_width = ref(null)
 const floor_height = ref(null)
 // const cabin_y = ref('0px')
-const cabin_floor = ref()
+const cabins_floor = ref([])
 const radio = ref()
 const radio_wrapper = ref()
-const cabin_direction = ref('')
-// const lift_cabin = ref()
+const cabin_direction = ref([])
 
-const toFloor = ref()
+
 function getshaftWidth(element) {
   shaft_width.value = element.clientWidth
 }
@@ -58,56 +59,87 @@ function getFloorHeight(element) {
 }
 
 const lift_queue = ref([])
-const cabin_status = ref('ready')
+const cabins_status = ref([])
 function queueColor(i) {
   radio_wrapper.value[i - 1].style.border = '1px solid red'
-    radio.value[i - 1].className = 'call'
+  radio.value[i - 1].className = 'call'
+}
+function chooseLift(floor) {
+  var times = []
+  for (let elevator = 0; elevator < shafts.value; elevator++) {
+    times.push(
+      {
+        time: Math.abs(floor - cabins_floor.value[elevator]) * 1000,
+        status: cabins_status.value[elevator],
+        id: elevator
+      })
+  }
+  times = times.filter(elevator => elevator.status != 'moving')
+  console.log(times)
+  // var minTime = Math.min(...times)
+  var minTime = times.reduce((i, ac) => {
+    return (i.time < ac.time ? i : ac);
+  });
+  console.log(minTime)
+  var res = {
+    elevator: minTime.id + 1,
+    floor: floor
+  }
+  console.log(res)
+  return res
+
 }
 function check_lift_queue(i) {
-  if ((lift_queue.value.length === 0 || lift_queue.value.at(-1) != i) && (i != cabin_floor.value || movingFloor.value != i)) {
-    lift_queue.value.push(i)
-    localStorage.setItem('queue', lift_queue.value)
-    console.log(store.getters.getCabinQueue)
+  if ((lift_queue.value.length === 0 || lift_queue.value.at(-1) != i) && (!cabins_floor.value.includes(i) || !moving_floor.value.includes(i))) {
+
+    lift_queue.value.push(chooseLift(i))
+    console.log(lift_queue.value)
+    localStorage.setItem('queue', JSON.stringify(lift_queue.value))
     queueColor(i)
-    console.log('lift_queue.value', lift_queue.value)
-    if (cabin_status.value === 'ready') {
-      call()
+
+    if (cabins_status.value[0] === 'ready') {
+    call()
     }
   }
 
 }
-const movingFloor = ref(1)
-const cabin_blink = ref()
+const moving_floor = ref([])
+const cabins_blink = ref([])
+
 async function call() {
-  cabin_status.value = 'moving'
-  console.log('status', cabin_status.value)
+
+  // console.log('status', cabins_status.value)
 
   var promise = new Promise((resolve) => {
-    movingFloor.value = lift_queue.value.shift()
-    console.log('floor', movingFloor.value)
-    var time = Math.abs(movingFloor.value - cabin_floor.value) * 1000
+    var i = lift_queue.value.shift()
+    var elevator = i.elevator
+    moving_floor.value[elevator - 1] = i.floor
+    cabins_status.value[elevator - 1] = 'moving'
+    var time = Math.abs(moving_floor.value[elevator - 1] - cabins_floor.value[elevator - 1]) * 1000
     console.log('time', time)
-    elevatorMove(movingFloor.value, time)
-    setTimeout(resolve, time)
+    elevatorMove(elevator, moving_floor.value[elevator - 1], time)
+
+    setTimeout(() => { resolve(elevator) }, time)
 
   })
-  await promise.then(() => {
-    cabin_status.value = 'waiting'
-    console.log('status', cabin_status.value)
-    cabin_blink.value = true
+  await promise.then((elevator) => {
+    console.log(elevator)
+    cabins_status.value[elevator - 1] = 'waiting'
+    console.log('status', cabins_status.value)
+    cabins_blink.value[elevator - 1] = true
     setTimeout(() => {
-      cabin_blink.value = false
-      cabin_status.value = 'ready'
-      console.log('status', cabin_status.value)
-      toFloor.value = ''
-      cabin_direction.value = ''
-      radio_wrapper.value[movingFloor.value - 1].style.border = '1px solid green'
+      cabins_blink.value[elevator - 1] = false
+      cabins_status.value[elevator - 1] = 'ready'
+      console.log('status', cabins_status.value)
+      cabin_direction.value[elevator - 1] = ''
+      radio_wrapper.value[moving_floor.value[elevator - 1] - 1].style.border = '1px solid green'
 
-      radio.value[movingFloor.value - 1].classList.remove("call")
+      radio.value[moving_floor.value[elevator - 1] - 1].classList.remove("call")
       if (lift_queue.value.length != 0) {
         call()
 
-      } else {
+      }
+      else {
         localStorage.removeItem('queue')
       }
     }, 3000)
@@ -115,19 +147,17 @@ async function call() {
 
 }
 
-async function elevatorMove(floor, time) {
-  // cabin_status.value = 'moving'
+async function elevatorMove(elevator, floor, time) {
+  // cabins_status.value = 'moving'
 
 
   var promise = new Promise((resolve) => {
 
-    var cabin = document.getElementById('cabin');
-    // lift_cabin.value.setFloor(floor)
-    toFloor.value = floor
-    if ((cabin_floor.value - floor) > 0) {
-      cabin_direction.value = '↓'
+    var cabin = document.getElementById(`cabin${elevator}`);
+    if ((cabins_floor.value[elevator - 1] - floor) > 0) {
+      cabin_direction.value[elevator - 1] = '↓'
     }
-    else cabin_direction.value = '↑'
+    else cabin_direction.value[elevator - 1] = '↑'
     var animation = cabin.animate([
       // { transform: `translateY(${cabin_y.value})` },
       { transform: `translateY(-${(floor - 1) * floor_height.value}px)` }
@@ -136,17 +166,15 @@ async function elevatorMove(floor, time) {
     animation.addEventListener('finish', function () {
       cabin.style.transform = `translateY(-${(floor - 1) * floor_height.value}px)`;
     });
-    setTimeout(resolve, time)
+    setTimeout(() => { resolve(elevator) }, time)
   })
 
   // })
   await promise.then(() => {
     // console.log(floor)
-    cabin_floor.value = floor
-    localStorage.setItem('cabin_floor', floor)
+    cabins_floor.value[elevator - 1] = floor
+    localStorage.setItem('cabins_floor', cabins_floor.value)
   })
-
-
 }
 
 onBeforeMount(() => {
@@ -159,19 +187,73 @@ onMounted(() => {
   // console.log(element.clientHeight)
   getshaftWidth(element)
   getFloorHeight(element)
-  if (localStorage.getItem('cabin_floor') != null){
-    cabin_floor.value = localStorage.getItem('cabin_floor')
-    var cabin = document.getElementById('cabin');
-    cabin.style.transform = `translateY(-${(cabin_floor.value - 1) * floor_height.value}px)`;
+  for (let i = 0; i < shafts.value; i++) {
+    // shaft.value[i].style.position = 'relative'
+    // document.getElementById(`shaft${i+1}`).style.position = 'relative'
+    document.getElementById(`cabin${i + 1}`).style.left = `${shaft_width.value * i}px`
+    // cabin.value[i].style.position = 'absolute'
 
-  } else {
-    cabin_floor.value = 1
+    // console.log(i)
   }
-  if (localStorage.getItem('queue') != null){
-    lift_queue.value = localStorage.getItem('queue').split(',')
-    if (lift_queue.value[0] === cabin_floor.value) {
-      lift_queue.value.shift()
+  cabins_floor.value = []
+  moving_floor.value = []
+  cabins_status.value = []
+  cabins_blink.value = []
+  cabin_direction.value = []
+  for (let i = 0; i < shafts.value; i++) {
+    cabins_floor.value.push(1)
+    moving_floor.value.push(1)
+    cabins_status.value.push('ready')
+    cabins_blink.value.push(false)
+    cabin_direction.value.push('')
+    // console.log(i)
+  }
+  if (localStorage.getItem('cabins_floor') != null) {
+    cabins_floor.value = localStorage.getItem('cabins_floor').split(',').map(Number)
+    moving_floor.value = localStorage.getItem('cabins_floor').split(',').map(Number)
+    
+
+    // cabins_floor.value.forEach((el) => {
+    //   el = Number(el)
+    //   console.log(el)
+    // })
+    // cabins_floor.value[0] = Number(cabins_floor.value[0])
+    console.log(cabins_floor.value)
+    for (let i = 0; i < cabins_floor.value.length; i++) {
+      var cabin = document.getElementById(`cabin${i + 1}`);
+      console.log(cabin)
+      cabin.style.transform = `translateY(-${(cabins_floor.value[i] - 1) * floor_height.value}px)`;
+      // console.log(cabin)
     }
+
+
+
+  }
+  // else {
+  //   cabins_floor.value = []
+  //   moving_floor.value = []
+  //   cabins_status.value = []
+  //   cabins_blink.value = []
+  //   cabin_direction.value = []
+  //   for (let i = 0; i < shafts.value; i++) {
+  //     cabins_floor.value.push(1)
+  //     moving_floor.value.push(1)
+  //     cabins_status.value.push('ready')
+  //     cabins_blink.value.push(false)
+  //     cabin_direction.value.push('')
+  //     // console.log(i)
+  //   }
+  //   // cabins_status.value[0] = 'moving'
+  //   console.log(cabins_status.value)
+  //   // cabins_floor.value = 1
+  // }
+  if (localStorage.getItem('queue') != null) {
+
+    lift_queue.value = JSON.parse(localStorage.getItem('queue'))
+
+    // if (lift_queue.value[0] === cabins_floor.value) {
+    // lift_queue.value.shift()
+    // }
     console.log(lift_queue.value)
     setTimeout(call, 1000)
   }
@@ -237,10 +319,14 @@ onMounted(() => {
 
 .cabin {
   position: absolute;
-  display: inline-block;
+  //display: inline-block;
   bottom: 0;
 
 
+}
+
+.cabin:last-of-type {
+  background-color: red;
 }
 
 .settings {
